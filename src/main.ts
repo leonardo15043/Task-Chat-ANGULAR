25b4052d8dd45c212486d7e8ada2bbefdf740f87
leonardo15043 <lahernandez15043@gmail.com>
2020-12-04 09:50:39 -0500

    Proyecto Inicial

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,7 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+let dateInit = new Date();
+  (1609304400000 >= dateInit.getTime())?
+    platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err)):''
